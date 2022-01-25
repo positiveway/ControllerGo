e78853eb936ad3b29e1e12af5346d8dbdb08078f
Andrew <PositiveWay05@gmail.com>
2022-01-25 16:02:01 +0400

    format check

--- wsocket.go
+++ wsocket.go
@@ -47,22 +47,26 @@ func mainWS() {
 		//	maxSize = nn
 		//}
 
-		msg_str := string(msg)
-		raw_events := strings.Split(msg_str, ";")
+		msgStr := string(msg)
 
-		bytesAmount := checkBytesAmount(raw_events[0], nn)
+		if msgStr[len(msgStr)-1] != ';' {
+			panic("Invalid message format")
+		}
+		rawEvents := strings.Split(msgStr, ";")
 
-		raw_events = raw_events[1 : len(raw_events)-1]
-		batch_str := ""
-		for _, raw_event := range raw_events {
-			rawSlice := strings.Split(raw_event, ",")
+		bytesAmount := checkBytesAmount(rawEvents[0], nn)
+
+		rawEvents = rawEvents[1 : len(rawEvents)-1]
+		batchStr := ""
+		for _, rawEvent := range rawEvents {
+			rawSlice := strings.Split(rawEvent, ",")
 			id, eventType, btnOrAxis, value := rawSlice[0], rawSlice[1], rawSlice[2], rawSlice[3]
 			event := makeEvent(id, eventType, btnOrAxis, value)
-			batch_str += fmt.Sprintf("%v;", event)
+			batchStr += fmt.Sprintf("%v;", event)
 		}
 
 		//fmt.Printf("Bytes: %v; Event: %s Host: %v\n", nn, msg, raddr)
-		fmt.Printf("Bytes: %v; Event: %s Host: %v\n", bytesAmount, batch_str, raddr)
+		fmt.Printf("Bytes: %v; Event: %s Host: %v\n", bytesAmount, batchStr, raddr)
 		//fmt.Printf("Max: %v\n", maxSize)
 
 	}
