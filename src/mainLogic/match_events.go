19eb83d0798a17e93346b3331a4793bdf4bfd303
Andrew <PositiveWay05@gmail.com>
2022-02-25 13:12:28 +0400

    removed compressed events

--- src/mainLogic/match_events.go
+++ src/mainLogic/match_events.go
@@ -2,34 +2,7 @@ package mainLogic
 
 import "fmt"
 
-func compressEvents(events Events) Events {
-	lastEvents := map[string]*Event{}
-
-	var compressedEvents Events
-
-	for _, event := range events {
-		switch event.eventType {
-		case EvAxisChanged:
-			switch event.btnOrAxis {
-			case
-				AxisLeftStickX,
-				AxisLeftStickY,
-				AxisRightStickX,
-				AxisRightStickY:
-				lastEvents[event.btnOrAxis] = &event
-				continue
-			}
-		}
-		compressedEvents = append(compressedEvents, event)
-	}
-	for _, event := range lastEvents {
-		compressedEvents = append(Events{*event}, compressedEvents...)
-	}
-	return compressedEvents
-}
-
 func matchEvents(events Events) {
-	//events = compressEvents(events)
 	for _, event := range events {
 		switch event.eventType {
 		case EvAxisChanged:
